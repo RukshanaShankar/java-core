c0b17f2eaa845341fbe1cd1cc1e9446c3e3b727e
RukshanaShankar <161870786+RukshanaShankar@users.noreply.github.com>
2024-03-11 22:33:14 +0530

    all files removed

--- src\:com\:learning\:core/Day1/D01P01.java
+++
@@ -1,9 +0,0 @@
-package com.learning.core.day1;
-
-public class TestBook {
-	    public static void main(String[] args) {
-	        Book myBook = BookOutput.createBook("Java Prgramming", 350.00);
-	        BookOutput.showBook(myBook);
-	    }
-	
-}
--- src\:com\:learning\:core/Day4/InvalidNameException.java
+++
@@ -1,9 +0,0 @@
-package com.learnin.core.day4;
-
-public class InvalidNameException extends Exception
-{
- public InvalidNameException()
- {
-	 System.out.println(" Invalid Applicant Name");
- }
-}
